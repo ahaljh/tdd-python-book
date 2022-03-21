a1124fa1f5d5d0a0c8fb710db1a7af4a133c66cb
ahaljh <ahaljh@gmail.com>
2022-03-21 17:10:45 +0900

    chaper2

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome('../resources/chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('../resources/chromedriver')
+        self.browser.implicitly_wait(3)
+ 
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Congratulations' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # add stories
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+    # unittest.main()
